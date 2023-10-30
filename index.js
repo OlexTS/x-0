c75602715e5f535a68051fa6ca4226a7ae95c992
OlexTS <sashokseven@gmail.com>
2023-10-30 22:05:03 +0200

    added checkWinners

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ content.addEventListener("click", onClick);
 restart.addEventListener("click", onClickBtn);
 
 let player = "X";
-const win = [
+const winners = [
   [1, 2, 3],
   [3, 6, 9],
   [4, 5, 6],
@@ -16,17 +16,47 @@ const win = [
 ];
 function createMarkup() {
   let markup = "";
-  for (i = 1; i <= 9; i += 1) {
+  for (let i = 1; i <= 9; i += 1) {
     markup += `<div class="item" data-id=${i}></div>`;
   }
   return markup;
 }
 content.insertAdjacentHTML("beforeend", createMarkup());
 
+function checkWinner() {
+  for (const line of winners) {
+    const [a, b, c] = line;
+    const cells = document.querySelectorAll(
+      `[data-id='${a}'], [data-id='${b}'], [data-id='${c}']`
+    );
+    const values = Array.from(cells).map((value) => value.textContent);
+    if (
+      values.every((val) => val === "X") ||
+      values.every((val) => val === "O")
+    ) {
+      return values[0];
+    }
+  }
+  return null;
+}
+function isDraw() {
+    const cells = document.querySelectorAll('.item');
+    const values = Array.from(cells).every(val => val.textContent !=='') 
+    return values
+}
 function onClick(e) {
   if (!e.target.textContent) {
     e.target.textContent = player;
     player = player === "X" ? "O" : "X";
+    const winner = checkWinner();
+    if (winner) {
+        content.innerHTML = createMarkup();
+      alert(`Player ${winner} won`);
+    }
+    else if (isDraw()) {
+        content.innerHTML = createMarkup();
+        alert('It is a draw')
+      }
   } else alert("Please enter your value in an empty cell");
 }
 function onClickBtn() {
